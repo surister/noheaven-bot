82f140de8855050b41cea1781d583da170a7d934
surister <surister98@gmail.com>
2018-11-21 16:33:16 +0100

    Add help fields for music in Fields class

--- noheavenbot/utils/constants.py
+++ noheavenbot/utils/constants.py
@@ -23,14 +23,27 @@ class Fields(NamedTuple):
                     ('__**Garch**__', 'Garch commands'),
                     ('!garch', 'Shows a random garch name'),
                     ('!garch save <name>', 'Saves new garch name'),
-                    ('!garch list', 'Shows the saved list')
+                    ('!garch list', 'Shows the saved list'),
+                    ('!music', 'Shows you the music options')
                     )
     music_fields = (
-        ('__**Musica**__', 'Comandos de musica'),
+        ('!music', 'muestra este mensaje'),
         ('!play <cancion>', 'Toca la <cancion>'),
-        ('!next', 'Salta a la siguiente cancion, si no hay ninguna espera 60 segundos'),
-        ('!stop', 'Cierra el bot')
-        ('')
+        ('!skip', 'Salta a la siguiente cancion, si no hay ninguna espera 60 segundos'),
+        ('!stop', 'Cierra el bot'),
+        ('!pause', 'La música se pausa'),
+        ('!resume', 'La musica continua'),
+        ('!volumen', 'Te dice el volumen actual'),
+        ('!join', 'Mueve el bot a tu canal (No necesario hacerlo, play lo hace automaticamente'),
+        ('!playlist', 'Te muestra las 5 primeras playlists que existen, con 3 canciones cada una'),
+        ('!playlist p <nombredeplaylist> <r>', 'Empieza a tocar la playlist, si añades la opcion "r", la toca random.'),
+        ('!playlist make <nombre>', 'Crea una playlist con <nombre> nombre.'),
+        ('!playlist add <nombre> <cancion>', 'Añade la canción <cancion> a la playlist <nombre>'),
+        ('!playlist del <nombre>', 'borra la playlist <nombre>'),
+        ('!playlist dels <nombre> <cancion>', 'Borra la canción <cancion> de la playlist <nombre>'),
+        ('!playlist temp', 'Te muestra la lista temporal actual, esta se crea con las canciones que tocas con !play'),
+        ('!playlist tcopy <nombre>', 'Crea una playlist <nombre> con las canciones de la playlsit temporal'),
+        ('**Información adicional**', 'El bot tiene un timeout de 60 segundos, si en 60 segundos no toca musica, se sale del canal'),
     )
     a = [
         ('test', 'boi'),
