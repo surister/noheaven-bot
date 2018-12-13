61bf5058f4441c2e867f832d7fade18b22fb0672
surister <surister98@gmail.com>
2018-12-13 17:11:32 +0100

    Pretify welcome message

--- noheavenbot/cogs/events/message_events.py
+++ noheavenbot/cogs/events/message_events.py
@@ -9,7 +9,6 @@ class OnMessage:
 
     @staticmethod
     async def on_message(message):
-        print('mesage event')
         with open(f'{Path.UTILS}/muted.json', 'r') as f:
             x = json.load(f)
         if message.author.id in x['users']:
