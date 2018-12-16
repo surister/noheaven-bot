4bc7e2e4e18238802243860661d00f88d16a64da
surister <surister98@gmail.com>
2018-12-16 13:57:42 +0000

    Prod -> Dev update

--- noheavenbot/cogs/commands/database_funcs.py
+++ noheavenbot/cogs/commands/database_funcs.py
@@ -9,16 +9,18 @@ class DatabaseInfoInjector:
     def __init__(self, bot):
         self.bot = bot
 
-    @command()
-    async def get_users(self, ctx):
-        conn = await asyncpg.connect('postgresql://root@localhost/noheaven')
-        async for member in ctx.guild.members:
-            await conn.execute('''
-            INSERT INTO users (id, name, date, admin) VALUES ($1, $2, $3, $4)
-            ''', member.id, member.display_name, member.joined_at)
-        await conn.close()
-    print('finished')
 
+
+    @command(name='use')
+    async def get_users(self, ctx):
+        db = await asyncpg.create_pool(**self.credentials)
+        for member in ctx.guild.members:
+            await db.execute('''
+            INSERT INTO users (id, name, date, admin) VALUES ($1, $2, $3, $4)
+            ''', member.id, member.display_name, member.joined_at, False)
+        await db.close()
 
 def setup(bot):
     bot.add_cog(DatabaseInfoInjector(bot))
+
+
