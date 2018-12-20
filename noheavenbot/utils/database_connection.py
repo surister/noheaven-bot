191bcfbcb153b1b952e9c6d44f89c140f7ececc3
surister <surister98@gmail.com>
2018-12-20 17:15:56 +0100

    Add and compoleted all garch tables functions

--- noheavenbot/utils/database_connection.py
+++ noheavenbot/utils/database_connection.py
@@ -1,5 +1,6 @@
 from noheavenbot import Vars
 from asyncpg import create_pool
+from random import randrange
 
 
 class DatabaseConnection:
@@ -13,24 +14,49 @@ class DatabaseConnection:
         return conn
 
 
-class DatabaseFunction:
+class GarchTable:
 
     @classmethod
-    async def insert_into_(cls, table: str):
+    async def get_max_index(cls):
+        conn = await DatabaseConnection.connect()
+        n = await conn.fetchval('''
+        SELECT MAX(index) FROM garch
+                        ''')
+        await conn.close()
+
+        return n
+
+    @classmethod
+    async def remove_row(cls, index: int):
+        conn = await DatabaseConnection.connect()
+        conn.execute('''
+        DELETE FROM garch where garch.index = $1
+        ''', index)
+
+    @classmethod
+    async def insert_row(cls, name: str):
         conn = await DatabaseConnection.connect()
         await conn.execute('''
-        INSERT INTO $1 VALUES ()
-        ''')
+        INSERT INTO garch VALUES ($1, $2) 
+        ''', name, await GarchTable.get_max_index() + 1)
+
+        await conn.close()
 
     @classmethod
-    async def insert_garch(cls, name: str):
+    async def fetch_name(cls):
+        n_rdm = randrange(0, await GarchTable.get_max_index())
         conn = await DatabaseConnection.connect()
-        await conn.execute('''
-        INSERT INTO garch VALUES ($1) 
-        ''', name)
+
+        name = await conn.fetchval('''
+        SELECT name FROM garch WHERE index = $1
+        ''', n_rdm)
+        await conn.close()
+        return name
 
     @classmethod
-    async def fetch_garch(cls):
+    async def fetch_all(cls):
         conn = await DatabaseConnection.connect()
-        await conn.fetchor
+        query = await conn.fetch('''
+        SELECT * FROM garch''')
 
+        return [f'{index}: {value}' for value, index in query]
