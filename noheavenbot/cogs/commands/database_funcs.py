d1aa9771e56438914a45063347de6d6d05527391
surister <surister98@gmail.com>
2018-12-20 17:17:32 +0100

    Add all garch commands

--- noheavenbot/cogs/commands/database_funcs.py
+++ noheavenbot/cogs/commands/database_funcs.py
@@ -1,23 +1,38 @@
-from noheavenbot.utils import DatabaseConnection
-from noheavenbot.utils.json_utils import Json
-from discord.ext.commands import command
+from noheavenbot.utils import DatabaseConnection, GarchTable
+from discord.ext.commands import command, group
 
 
 class DatabaseInfoInjector:
     def __init__(self, bot):
         self.bot = bot
 
-    @command(name='use')
-    async def get_users(self, ctx):
-        conn = await DatabaseConnection.connect()
-        for i, name in enumerate(Json.get('/home/surister/noheavenbot/noheavenbot/utils/garch.json')['name']):
-            await conn.execute('''
-            INSERT INTO garch (name, index) VALUES ($1, $2)
-            ''', name, i)
-        await conn.close()
+    @group(invoke_without_command=True)
+    async def garch(self, ctx):
+        await ctx.send(await GarchTable.fetch_name())
+
+    @garch.command()
+    async def save(self, ctx, *, name):
+        await GarchTable.insert_row(name)
+        await ctx.send(f'Gardado: {name}')
+
+    @garch.command(name='del')
+    async def _del(self, ctx, index):
+        try:
+            index = int(index)
+        except ValueError:
+            return await ctx.send('')
+
+        if index <= await GarchTable.get_max_index():
+            return await ctx.send('Ese index no existe')
+        await GarchTable.remove_row(index)
+
+    @garch.command()
+    async def list(self, ctx):
+
+        name_list = await GarchTable.fetch_all()
+        name_list.insert(0, '\u200b')
+        await ctx.send('\n'.join(name_list))
 
 
 def setup(bot):
     bot.add_cog(DatabaseInfoInjector(bot))
-
-# DO ABOVE
