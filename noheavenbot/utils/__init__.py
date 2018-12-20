582c7b027b86b64d748e9fe75bc6831446d24ece
surister <surister98@gmail.com>
2018-12-20 17:16:36 +0100

    Add import to __all__

--- noheavenbot/utils/__init__.py
+++ noheavenbot/utils/__init__.py
@@ -1,3 +1,4 @@
 from noheavenbot.utils.database_connection import DatabaseConnection
+from noheavenbot.utils.database_connection import GarchTable
 
-__all__ = ['DatabaseConnection']
+__all__ = ['DatabaseConnection', 'GarchTable']
