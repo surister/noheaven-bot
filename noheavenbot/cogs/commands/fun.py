151858df171e2d0b775ab3d90ba49bb24f3f38b7
surister <surister98@gmail.com>
2018-12-20 17:16:52 +0100

    Deleted Garch commands

--- noheavenbot/cogs/commands/fun.py
+++ noheavenbot/cogs/commands/fun.py
@@ -1,50 +1,4 @@
-import json
-import random
-
-from discord.ext.commands import command, group
-from noheavenbot.utils.constants import Path
-
-
-class Garch:
-    # Todo use utils json commands
-    def __init__(self, bot):
-        self.bot = bot
-
-    @group(invoke_without_command=True)
-    async def garch(self, ctx):
-        with open(f'{Path.UTILS}/garch.json', 'r') as gr:
-            x = json.load(gr)
-        await ctx.channel.send(random.choice(x['name']))
-
-    @garch.command()
-    async def save(self, ctx, *, arg):
-        with open(f'{Path.UTILS}/garch.json', 'r') as gr:
-            x = json.load(gr)
-
-        x['name'].append(arg)
-
-        with open(f'{Path.UTILS}/garch.json', 'w') as rg:
-            json.dump(x, rg, indent=1)
-
-    @garch.command()
-    async def list(self, ctx):
-        with open(f'{Path.UTILS}/garch.json', 'r') as gr:
-            x = json.load(gr)
-        await ctx.send(x['name'])
-
-    @garch.command(name='delete')
-    async def _delete(self, ctx, index: int):
-        with open(f'{Path.UTILS}/garch.json', 'r') as gr:
-            x = json.load(gr)
-
-        fmt = x['name'][index]
-
-        del x['name'][index]
-
-        with open(f'{Path.UTILS}/garch.json', 'w') as rg:
-            json.dump(x, rg, indent=1)
-
-        await ctx.send(f'Borrado {fmt}')
+from discord.ext.commands import command
 
 
 class Text:
@@ -65,5 +19,5 @@ class Text:
 
 
 def setup(bot):
-    bot.add_cog(Garch(bot))
+
     bot.add_cog(Text(bot))
