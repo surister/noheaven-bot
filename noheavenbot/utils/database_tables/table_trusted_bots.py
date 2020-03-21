a697e141e0047503a09d69ba748deec0b77e90f2
surister <surister98@gmail.com>
2020-03-21 19:15:01 +0100

    Modify utils + add table-bots + refractor validators.py

---
+++ noheavenbot/utils/database_tables/table_trusted_bots.py
@@ -0,0 +1,14 @@
+from noheavenbot.utils import Database
+from datetime import datetime
+
+
+class TrustedBot:
+
+    @classmethod
+    async def insert_bot(cls, added_by_id: str, added_by_name: str, bot_id: str, bot_name: str, is_admin: bool = False):
+        conn = await Database.connect()
+        await conn.execute('''
+        INSERT INTO nh.trusted_bots 
+            (added_by_id, added_by_name, bot_id, bot_name, date_arrival, is_admin) 
+            VALUES ($1, $2, $3, $4, $5, $6)
+        ''', added_by_id, added_by_name, bot_id, bot_name, datetime.today(), is_admin)
