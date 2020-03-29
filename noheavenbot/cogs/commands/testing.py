15894cf1101d3139c6587eb6e92c4b9ce37b609e
surister <surister98@gmail.com>
2020-03-29 14:41:57 +0200

    np

--- noheavenbot/cogs/commands/testing.py
+++ noheavenbot/cogs/commands/testing.py
@@ -2,6 +2,7 @@ from discord.ext.commands import command, Cog
 from noheavenbot.utils.constants import TEXTCHANNELS
 from discord import Member
 from noheavenbot.utils.database_tables.table_users import Users
+from noheavenbot.utils.validator import has_role as check_role
 
 
 class Test(Cog):
@@ -11,14 +12,11 @@ class Test(Cog):
 
     @command()
     async def test(self, ctx):
-        values = []
-        if len(ctx.message.guild.members) > await Users.len():
-            db_user_list = await Users.get_users_identifiers_list()
-            for d_user in ctx.message.guild.members:
-                if not db_user_list.__contains__(str(d_user.id)):
-                    values.append((d_user.name, d_user.bot, int(d_user.discriminator), str(d_user.id)))
-
-            await Users.insert_many(values)
+        if check_role(ctx.message.author.roles, 445947005169303552, True):
+            admin = True
+            return await ctx.send('all ok')
+        else:
+            return await ctx.send('Solo los administradores pueden añadir bots con permisos de administración.')
 
 
 def setup(bot):
